3bc41bc1993268db9a440aafc47b2cf714fde620
gmars13 <34511907+gmars13@users.noreply.github.com>
2022-01-03 21:36:13 +0800

    Update db.config.js

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: process.env.NODE_ENV === "development" ? "localhost": process.env.DB_HOST,
-    USER: process.env.NODE_ENV === "development" ? "root": process.env.DB_USER,
-    PASSWORD: process.env.NODE_ENV === "development" ? "": process.env.DB_PASSWORD,
-    DB: process.env.NODE_ENV === "development" ? "shopify_custom_options": process.env.DB_SCHEMA
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_SCHEMA
 };
